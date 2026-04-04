6baffbd0e694e88262a9cb3cf4efcd2cf07d95fc
Bankole Ebenezer <bankoleebenezer111@gmail.com>
2026-04-04 16:44:44 -0700

    update the validation

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,12 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const recipesController = require('../controllers/recipes');
+const { recipeRules, validate } = require('../middleware/validator');
 
 router.get('/', recipesController.getAll);
 router.get('/:id', recipesController.getSingle);
 
 router.post('/', recipesController.createRecipes)
 router.put('/:id', recipesController.updateRecipe);
+router.post('/', recipeRules, validate, recipesController.createRecipes);
+router.put('/:id', recipeRules, validate, recipesController.updateRecipe);
 router.delete('/:id', recipesController.deleteRecipe);
 
 module.exports = router;
